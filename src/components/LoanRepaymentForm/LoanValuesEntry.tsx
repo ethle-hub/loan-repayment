adba52fb791f632c7af1ef46f38f16a9ccafb9cb
ele <ethle.atwork@gmail.com>
2021-03-09 00:05:44 +1100

    collect loan values in component's state

--- src/components/LoanRepaymentForm/LoanValuesEntry.tsx
+++ src/components/LoanRepaymentForm/LoanValuesEntry.tsx
@@ -12,16 +12,18 @@ import Container from "@material-ui/core/Container";
 
 import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
 
-
+// input type for this component
 type LoanValuesEntryProps = {
-  loanAmount: number,
-  interestRate: number,
-  loanTermInYear: number,
-  expectedPaymentsPerYear: number,
-  startDate: Date,
-  extraLoanPaymentAmount: number 
-}
+  loanAmount: number;
+  interestRate: number;
+  loanTermInYear: number;
+  expectedPaymentsPerYear: number;
+  startDate: Date;
+  extraLoanPaymentAmount?: number | undefined;
+  onClick(e: React.MouseEvent<HTMLElement>): void;
+};
 
+// component stype is here
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
@@ -38,7 +40,11 @@ const useStyles = makeStyles((theme: Theme) =>
       width: "100%", // Fix IE11 issue.
       mt: 3,
     },
-    botton_submit: { mt: 3, mb: 2 },
+    botton_submit: {
+      mt: 3,
+      mb: 2,
+      margin: theme.spacing(1),
+    },
     paper: {
       padding: theme.spacing(2),
       textAlign: "center",
@@ -47,17 +53,41 @@ const useStyles = makeStyles((theme: Theme) =>
   })
 );
 
-export default function LoanValuesEntry(initialProps : LoanValuesEntryProps) {
+// funtional component
+export default function LoanValuesEntry(props: LoanValuesEntryProps) {
+  // createStyles() -> makeStyles()  to be use it here e.g. useStyle() :)
   const classes = useStyles();
 
-  const [loanValues, setLoanValues] = React.useState<LoanValuesEntryProps>(initialProps);
+  /* make a copy of compument's state, consider good rule of efficient state management
+   * e.g. Getter and Setter for `loanValues` variable
+   * !Notes the setLoanValues() violates the `One Concern` rule because one state variable `loanValues` is responsible multiple values (e.g. in LoanValuesEntryProps)
+   */
+  const [loanValues, setLoanValues] = React.useState<LoanValuesEntryProps>(
+    props
+  );
 
+  // define the only function to update component's state
   const loanValuesOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     event.preventDefault();
-    console.log(event.target.name, event.target.value);    
+
+    // This function expect an change event from an `target` type HTMLInputElement where it is identified by `name` and hold the data in `value`
+    console.log(event.target.name, event.target.value);
+
+    /* ! This looks simple but you might want to avoid
+     * Here, you need nearby the whole state e.g. `...loanValues` to be able to update just `event.target.name`
+     * This is a big construction to invoke to simply increase a counter: all because the state variable is responsible for more than one concern
+     * 
+     * Q: what if I have way too much useState() variables?
+     * A: there is a good chance that you still violate the same rule e.g. Single Responsibility Principle. Perhaps the compoment will need to be splited. 
+     * 
+     * Make your own judgement or `Extract multiple state operations into a reducer.`
+     * 
+     * Use custom hook or a reducer for update logic but do in t away that less details of how the state is updated a desirable
+     * 
+     */
     setLoanValues({
       ...loanValues,
-      [event.target.name]: event.target.value
+      [event.target.name]: event.target.value,
     });
   };
 
@@ -74,88 +104,99 @@ export default function LoanValuesEntry(initialProps : LoanValuesEntryProps) {
         </Typography>
         <Box
           component="form"
-          // noValidate          
+          // noValidate
         >
           {/* entry fields */}
-          <form  className={classes.form} noValidate autoComplete="off">
-          <Grid container spacing={0}>           
-            <Grid item xs={8}>
-              <TextField
-                name="loanAmount"
-                required
-                fullWidth
-                id="loanAmount"
-                label="Loan amount"
-                defaultValue={loanValues.loanAmount}
-                onChange={loanValuesOnChange}
-                autoFocus
-              />
-            </Grid>
-            <Grid item xs={8}>
-              <TextField
-                required
-                fullWidth
-                id="interestRate"
-                label="Interest rate"
-                name="interestRate"
-                defaultValue={loanValues.interestRate}
-                onChange={loanValuesOnChange}
-              />
-            </Grid>
-            <Grid item xs={4} container justify="flex-end">
-              <Grid item>
-                <Link
-                  href="https://www.finder.com.au/bank-interest-rates"
-                  variant="body2"
+          <form className={classes.form} noValidate autoComplete="off">
+            <Grid container spacing={0}>
+              <Grid item xs={8}>
+                <TextField
+                  name="loanAmount"
+                  required
+                  fullWidth
+                  id="loanAmount"
+                  label="Loan amount"
+                  defaultValue={loanValues.loanAmount}
+                  onChange={loanValuesOnChange}
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={8}>
+                <TextField
+                  required
+                  fullWidth
+                  id="interestRate"
+                  label="Interest rate"
+                  name="interestRate"
+                  defaultValue={loanValues.interestRate}
+                  onChange={loanValuesOnChange}
+                />
+              </Grid>
+              <Grid item xs={4} container justify="flex-end">
+                <Grid item>
+                  <Link
+                    href="https://www.finder.com.au/bank-interest-rates"
+                    variant="body2"
+                  >
+                    Compare Bank Interest Rates
+                  </Link>
+                </Grid>
+              </Grid>
+              <Grid item xs={8}>
+                <TextField
+                  required
+                  fullWidth
+                  id="loanTermInYear"
+                  label="Loan term in years"
+                  name="loanTermInYear"
+                  defaultValue={loanValues.loanTermInYear}
+                  onChange={loanValuesOnChange}
+                />
+              </Grid>
+              <Grid item xs={8}>
+                <TextField
+                  required
+                  fullWidth
+                  id="expectedPaymentsPerYear"
+                  label="Payments made per year"
+                  name="expectedPaymentsPerYear"
+                  defaultValue={loanValues.expectedPaymentsPerYear}
+                  onChange={loanValuesOnChange}
+                />
+              </Grid>
+              <Grid item xs={8}>
+                <TextField
+                  required
+                  fullWidth
+                  id="startDate"
+                  label="Loan repayment start date"
+                  name="startDate"
+                  defaultValue={loanValues.startDate}
+                  onChange={loanValuesOnChange}
+                />
+              </Grid>
+              <Grid item xs={8}>
+                <TextField
+                  fullWidth
+                  id="extraLoanPaymentAmount"
+                  label="Optional extra payments"
+                  name="extraLoanPaymentAmount"
+                  defaultValue={loanValues.extraLoanPaymentAmount}
+                  onChange={loanValuesOnChange}
+                />
+              </Grid>
+              <Grid item xs={8}>
+                <Button
+                  variant="contained"
+                  color="default"
+                  className={classes.botton_submit}
+                  onClick={props.onClick}
+                  // startIcon={<CloudUploadIcon />}                  
                 >
-                  Compare Bank Interest Rates
-                </Link>
+                  Show Repayment
+                </Button>
               </Grid>
             </Grid>
-            <Grid item xs={8}>
-              <TextField
-                required
-                fullWidth
-                id="loanTermInYear"
-                label="Loan term in years"
-                name="loanTermInYear"
-                defaultValue={loanValues.loanTermInYear}
-                onChange={loanValuesOnChange}
-              />
-            </Grid>
-            <Grid item xs={8}>
-              <TextField
-                required
-                fullWidth
-                id="expectedPaymentsPerYear"
-                label="Payments made per year"
-                name="expectedPaymentsPerYear"
-                defaultValue={loanValues.expectedPaymentsPerYear}
-                onChange={loanValuesOnChange}
-              />
-            </Grid>
-            <Grid item xs={8}>
-              <TextField
-                required
-                fullWidth
-                id="startDate"
-                label="Loan repayment start date"
-                name="startDate"
-                defaultValue={loanValues.startDate}
-                onChange={loanValuesOnChange}
-              />
-            </Grid>
-            <Grid item xs={8}>
-              <TextField
-                fullWidth
-                id="extraLoanPaymentAmount"
-                label="Optional extra payments"
-                name="extraLoanPaymentAmount"
-                defaultValue={loanValues.extraLoanPaymentAmount}
-                onChange={loanValuesOnChange}
-              />
-            </Grid>            
-          </Grid>
           </form>
         </Box>
       </Box>
@@ -163,3 +204,14 @@ export default function LoanValuesEntry(initialProps : LoanValuesEntryProps) {
     </Container>
   );
 }
+
+// IMHO, having default props is quite handy (for functional component in react one can de define as below)
+LoanValuesEntry.defaultProps = {
+  loanAmount: 0,
+  interestRate: 0,
+  loanTermInYear: 0,
+  expectedPaymentsPerYear: 0,
+  startDate: Date(),
+  extraLoanPaymentAmount: 0,
+  onClick: (event: React.MouseEvent<HTMLElement>) => console.log(event),
+};
--- src/components/LoanRepaymentForm/index.tsx
+++ src/components/LoanRepaymentForm/index.tsx
@@ -27,7 +27,8 @@ export default function LoanEntryForm() {
       <Grid container spacing={3}>
         <Grid item xs={12} sm={6}>
           <Paper className={classes.paper}>
-          <LoanValuesEntry loanAmount={250000} interestRate={3} loanTermInYear={30} expectedPaymentsPerYear={12} startDate={new Date()} extraLoanPaymentAmount={25000}/>
+          {/* <LoanValuesEntry loanAmount={250000} interestRate={3} loanTermInYear={30} expectedPaymentsPerYear={12} startDate={new Date()} extraLoanPaymentAmount={25000}/> */}
+          <LoanValuesEntry />
           </Paper>
         </Grid>
         <Grid item xs={12} sm={6}>
